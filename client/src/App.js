cd7518bb954914de9e29e9c1f277ca693796fdb7
Meghanath Shetty <meghanshetty3122@gmail.com>
2024-06-06 16:12:55 +0530

    react-dom

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import InterviewPage from './pages/InterviewPage'; // Import the InterviewPage component
+import FaceRecognition from './pages/FaceRecognition';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/interview" element={<InterviewPage />} />
+        <Route path="/testing" element={<FaceRecognition />} />
       </Routes>
     </Router>
   );
