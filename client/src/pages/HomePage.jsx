c9ea0efedac93a0bf90513728460574b4b2bed36
Rakshith Rangaraj poojary <poojaryrakshith042@gmail.com>
2024-06-06 16:01:19 +0530

    adding  ...

---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../css/HomePage.css';
+
+
+function HomePage({storage, app}) {
+  return (
+    <div className='Home-div'>
+      
+      <div className='header-div'>
+      <h3>Mock Interview</h3>  
+      </div>
+      <div className='context-div'>
+        <h4>Land your dream job by prepping for interviews from the comfort of your couch.</h4>
+        <button>Get Started</button>
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
