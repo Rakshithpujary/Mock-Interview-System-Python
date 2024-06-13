5960e034da109ad25811d9d78f4ada88bff737d6
Meghanath Shetty <meghanshetty3122@gmail.com>
2024-06-13 22:55:37 +0530

    ..

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import InterviewPage from './pages/InterviewPage'; // Import the InterviewPage component
-<<<<<<< HEAD
-// import FaceRecognition from './pages/FaceRecognition';
-=======
->>>>>>> 2a60e916dbcf9c49cb0a8446411563cda21693a1
 import { GlobalProvider } from './components/utils/GlobalState';
 import Testing from './components/Testing';
 
@@ -16,11 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/interview" element={<InterviewPage />} />
-<<<<<<< HEAD
-          {/* <Route path="/testing" element={<FaceRecognition />} /> */}
-=======
           <Route path="/testing" element={<Testing />} />
->>>>>>> 2a60e916dbcf9c49cb0a8446411563cda21693a1
         </Routes>
       </GlobalProvider>
     </Router>
